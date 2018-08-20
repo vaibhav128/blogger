067db34a654f9d4757d4e21603218f2bcf7e14e5
Vaibhav Bhatia <vaibhavbhatia604@gmail.com>
2018-08-20 20:19:19 +0530

    refractored all-post.js file from jquery to JavaScript

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -1,29 +1,59 @@
-function loadPosts() {
-  $.get("/api/posts", (posts) => {
-    for (let p of posts) {
-      $("#posts-container").append(
-        $(`
-          <div class="col-4">
-            <div class="card m-2">
-              <div class="card-body">
-                <h5 class="card-title">${p.title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${
-                  p.user.username
-                }</h6>
-                <p class="card-text">
-                  ${p.body.substr(0, 200)}
-                  <a href="#">...read more</a>
-                </p>
-                <a href="#" class="card-link">Comment</a>
-                <a href="#" class="card-link">Like</a>
-              </div>
-            </div>
-          </div>
+// function loadPosts() {
+//   $.get("/api/posts", (posts) => {
+//     for (let p of posts) {
+//       $("#posts-container").append(
+//         $(`
+//           <div class="col-4">
+//             <div class="card m-2">
+//               <div class="card-body">
+//                 <h5 class="card-title">${p.title}</h5>
+//                 <h6 class="card-subtitle mb-2 text-muted">${
+//                   p.user.username
+//                 }</h6>
+//                 <p class="card-text">
+//                   ${p.body.substr(0, 200)}
+//                   <a href="#">...read more</a>
+//                 </p>
+//                 <a href="#" class="card-link">Comment</a>
+//                 <a href="#" class="card-link">Like</a>
+//               </div>
+//             </div>
+//           </div>
           
-          `)
-      );
-    }
-  });
+//           `)
+//       );
+//     }
+//   });
+// }
+
+//  refractor from jQuery to javascript
+
+function loadPosts() {
+  fetch("/api/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      for (let p of posts) {
+        getDomElementNode("#posts-container").insertAdjacentHTML(
+          "beforeend",
+          `
+      <div class="col-4">
+      <div class="card m-2">
+        <div class="card-body">
+          <h5 class="card-title">${p.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+          <p class="card-text">
+            ${p.body.substr(0, 200)}
+            <a href="#">...read more</a>
+          </p>
+          <a href="#" class="card-link">Comment</a>
+          <a href="#" class="card-link">Like</a>
+        </div>
+      </div>
+    </div>
+  `
+        );
+      }
+    });
 }
 
 loadPosts();
