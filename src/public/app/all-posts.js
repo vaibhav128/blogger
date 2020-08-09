b161035ef39dddfd850ea1f014a401e53a4f723c
Vaibhav Bhatia <vaibhavbhatia604@gmail.com>
2020-08-10 02:35:21 +0530

    Code Refractored form jquery to vanilla Js in all-posts and common js files

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -1,13 +1,15 @@
 function loadPosts() {
-    $.get('/api/posts', (posts) => {
-      for (let p of posts) {
-        $('#posts-container').append(
-          $(`
+  $.get("/api/posts", (posts) => {
+    for (let p of posts) {
+      $("#posts-container").append(
+        $(`
           <div class="col-4">
             <div class="card m-2">
               <div class="card-body">
                 <h5 class="card-title">${p.title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">${
+                  p.user.username
+                }</h6>
                 <p class="card-text">
                   ${p.body.substr(0, 200)}
                   <a href="#">...read more</a>
@@ -19,7 +21,9 @@ function loadPosts() {
           </div>
           
           `)
-        )
-      }
-    })
-  }
+      );
+    }
+  });
+}
+
+loadPosts();
--- src/public/app/common.js
+++ src/public/app/common.js
@@ -1,25 +1,75 @@
-$(() => {
-    $('#navbar').load('/components/navbar.html', loginIfNeeded)
-    $('#footer').load('/components/footer.html')
-    $('#content').load('/components/all-posts.html') // this contains it's own scripts too
-  })
-  
-  function loginIfNeeded() {
-    window.currentUser = window.localStorage.user
-      ? JSON.parse(window.localStorage.user)
-      : null
-    if (!currentUser) {
-      $.post('/api/users', {}, (user) => {
-        if (user) {
-          console.log('registered current user as ', user.username)
-          window.localStorage.user = JSON.stringify(user)
-          currentUser = user
-          $('#nav-username').text(currentUser.username)
-        }
+
+function loginIfNeeded() {
+  window.currentUser = window.localStorage.user
+    ? JSON.parse(window.localStorage.user)
+    : null;
+  if (!currentUser) {
+    fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
       })
-    } else {
-      console.log('resuming session as ', currentUser.username)
-      console.log($('#nav-username'))
-      $('#nav-username').text(currentUser.username)
-    }
+      .then((user) => {
+        console.log(user);
+        if (user) {
+          console.log("registered current user as ", user.username);
+          window.localStorage.user = JSON.stringify(user);
+          currentUser = user;
+          document.querySelector("#nav-username").textContent =
+            currentUser.username;
+        }
+      });
+  } else {
+    console.log("resuming session as ", currentUser.username);
+    document.querySelector("#nav-username").textContent = currentUser.username;
   }
+}
+
+// Refracted code omitted jquery
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log(event);
+  loader("#navbar", "/components/navbar.html", loginIfNeeded);
+  loader("#footer", "/components/footer.html");
+  loader("#content", "/components/all-posts.html");
+});
+
+
+const setInnerHTML = function (element, html) {
+  element.innerHTML = html;
+  Array.from(element.querySelectorAll("script")).forEach((oldScript) => {
+    const newScript = document.createElement("script");
+    Array.from(oldScript.attributes).forEach((attr) =>
+      newScript.setAttribute(attr.name, attr.value)
+    );
+    newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+    oldScript.parentNode.replaceChild(newScript, oldScript);
+  });
+};
+
+const fetcher = (path, element, ...restArgs) => {
+  fetch(`${path}`)
+    .then((response) => response.text())
+    .then((html) => {
+      setInnerHTML(element, html);
+    })
+    .then(() => {
+      restArgs.forEach((arg) => {
+        arg();
+      });
+    })
+    .catch((err) => {
+      console.log("error occured in fetcher", err);
+    });
+};
+
+const loader = (query, path, ...restArgs) => {
+  // used to load html in the query element
+  const element = document.querySelector(query);
+  fetcher(path, element, ...restArgs);
+};
