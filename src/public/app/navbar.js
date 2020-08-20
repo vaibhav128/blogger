2944d59f60346e1aa1df4b384c052f59e9f56778
Vaibhav Bhatia <vaibhavbhatia604@gmail.com>
2020-08-20 22:19:52 +0530

    refracted jquery code to javaScript in navbar.js

--- src/public/app/navbar.js
+++ src/public/app/navbar.js
@@ -1,9 +1,20 @@
 
-let navlinks = $('.navbar-nav .nav-link')
+// let navlinks = $('.navbar-nav .nav-link')
 
-navlinks.click((ev) => {
+// navlinks.click((ev) => {
 
-  let componentUrl = `/components/${$(ev.target).attr('data-component')}.html`
-  $('#content').load(componentUrl)
+//   let componentUrl = `/components/${$(ev.target).attr('data-component')}.html`
+//   $('#content').load(componentUrl)
 
+// })
+
+let navlinks = document.querySelectorAll(".navbar-nav .nav-link");
+console.log(navlinks);
+navlinks.forEach(function(currentLink){
+  currentLink.addEventListener("click", (event) => {
+    console.log(event);
+    let componentUrl = `/components/${event.target.dataset.component}.html`;
+    console.log(componentUrl);
+    loader("#content", componentUrl);
+  });
 })
